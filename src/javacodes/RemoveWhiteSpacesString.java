c7978d0a40ff2203e12d54b99020cd0d016d3da4
Rohit Sharma <rohitkumarsharma221@gmail.com>
2024-02-23 14:26:10 +0530

    Remove White Spaces in a given String

---
+++ src/javacodes/RemoveWhiteSpacesString.java
@@ -0,0 +1,23 @@
+package javacodes;
+
+public class RemoveWhiteSpacesString {
+
+    public static void main(String[] args) {
+        String str = "Hello World!";
+
+
+        //Approach One using Character.isWhitespace Method
+        StringBuilder result = new StringBuilder();
+        for(int i=0;i<str.length();i++){
+            if(!Character.isWhitespace(str.charAt(i))){
+                result.append(str.charAt(i));
+            }
+        }
+        System.out.println(result);
+
+        //Approach Two using replaceAll Method
+
+        String resultStr = str.replaceAll("\\s","");
+        System.out.println(resultStr);
+    }
+}
